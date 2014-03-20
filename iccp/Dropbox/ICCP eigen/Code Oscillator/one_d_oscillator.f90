8c54c9d9af680c0a3643a5291f6a81d68a5f7054
Stijn <stijn.balk@gmail.com>
2014-03-20 22:20:54 +0100

    walkers equilibreren

--- iccp/Dropbox/ICCP eigen/Code Oscillator/one_d_oscillator.f90
+++ iccp/Dropbox/ICCP eigen/Code Oscillator/one_d_oscillator.f90
@@ -4,30 +4,64 @@ program one_d_oscillator
   use plplot
   implicit none
   
-  integer, parameter :: N = 400
+  integer, parameter :: N = 400, steps = 1000
+  real(8) :: d, alpha
 !   integer ::
 !   integer, allocatable ::
-!   real(8) :: 
   real(8), allocatable :: walkers(:)
-  
+  alpha = 0.5_8
+  d = 1._8
   allocate(walkers(N))
-  
-  call walk(N, walkers)
-  print*, walkers
+
+  call init_walkers
+  call walk 
   
  contains
-
- subroutine walk(N, walkers)
-  integer, intent(in) :: N
-  integer :: i
-  real(8), intent(out) :: walkers(N)
-  real(8) :: r
+ 
+  subroutine walk
+    integer :: i, j, counter
+    real(8) :: kans, ran(N), p(N), walkers_new(N)
+    
+    d = 0.5_8
+    ran = 0._8
+    p = 0._8
+    kans = 0._8
+    walkers_new = 0._8
+    
+    do j = 1, steps
+      call random_number(ran)
+      ran = ran - 0.5_8
+      walkers_new = walkers + d * ran
+      p = (wavefct(walkers_new) / wavefct(walkers))**2
+    
+    do i = 1, N
+      if (p(i) .gt. 1._8) then
+        walkers_new(i) = walkers(i)
+        counter = counter + 1
+      else
+        call random_number(kans)
+        if (kans .le. p(i)) then
+          walkers_new(i) = walkers(i)
+          counter = counter + 1
+        endif
+      endif
+    end do
+    if (mod (j, 100) == 0) then
+      print *,counter, d
+      d = d * 2 * counter/(N-1)/100
+      counter = 0
+    end if
+    end do
+  end subroutine
   
-  r = 0._8
-  do i = 1, N
-    call random_number(r)
-    walkers(i) = r-0.5_8
-  end do
-end subroutine
+  subroutine init_walkers
+    call random_number(walkers)
+    walkers = walkers - 0.5_8
+  end subroutine
+  
+  function wavefct(x)
+    real(8) :: wavefct(N), x(N)
+      wavefct = exp(-alpha*x**2)
+  end function
 
 end program
