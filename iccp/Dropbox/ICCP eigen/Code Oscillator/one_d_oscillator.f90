70966b4558f03be9fe4c396df036034e2002c5f6
Stijn <stijn.balk@gmail.com>
2014-03-27 00:24:29 +0100

    met correctie van Jos, waarde bij alpha 0.6 klopt nog niet

--- iccp/Dropbox/ICCP eigen/Code Oscillator/one_d_oscillator.f90
+++ iccp/Dropbox/ICCP eigen/Code Oscillator/one_d_oscillator.f90
@@ -1,35 +1,33 @@
 program one_d_oscillator
 
-  use plot
-  use plplot
   implicit none
   
-  integer, parameter :: N = 400, steps = 1000
+  integer, parameter :: N = 400, steps = 10000
   real(8) :: d, alpha, E
   real(8), allocatable :: walkers(:)
-  alpha = 0.5_8
+  alpha = 0.6_8
   d = 1._8
   E = 1._8
   allocate(walkers(N))
   
 
   call init_walkers
-  call walk 
+  call aanpassen 
   E = sum(wavefct(walkers)**2 * localE(walkers)) / sum(wavefct(walkers)**2)
-  print *, E
   
   
  contains    
  
-  subroutine walk
+  subroutine aanpassen
     integer :: i, j, counter
-    real(8) :: kans, ran(N), p(N), walkers_new(N)
+    real(8) :: kans(N), ran(N), p(N), walkers_new(N), Etot
     
     d = 0.5_8
     ran = 0._8
     p = 0._8
     kans = 0._8
     walkers_new = 0._8
+    Etot = 0._8
     
     do j = 1, steps
       call random_number(ran)
@@ -37,27 +35,22 @@ program one_d_oscillator
       walkers_new = walkers + d * ran
       p = (wavefct(walkers_new) / wavefct(walkers))**2
     
-    do i = 1, N
-      if (p(i) .gt. 1._8) then
-        walkers_new(i) = walkers(i)
-        counter = counter + 1
-      else
-        call random_number(kans)
-        if (kans .le. p(i)) then
-          walkers_new(i) = walkers(i)
+      call random_number(kans)
+      do i = 1, N
+        if (p(i) .gt. kans(i)) then
+          walkers(i) = walkers_new(i)
           counter = counter + 1
-        endif
-      endif
+        end if
+      end do
+    
+      if (mod (j, 100) == 0) then
+        print *,counter, d
+        d = d * 2 * counter/(N-1)/100
+        counter = 0
+      end if
+      Etot = Etot + sum(localE(Walkers))
     end do
-    
-    if (mod (j, 100) == 0) then
-      print *,counter, d
-      d = d * 2 * counter/(N-1)/100
-      counter = 0
-    end if
-    
-    end do
-    
+    print *, 'final energy', etot/steps/N
   end subroutine
   
   subroutine init_walkers
